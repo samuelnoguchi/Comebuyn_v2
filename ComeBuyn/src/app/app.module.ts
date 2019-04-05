72f60a362031b8ef8ddd4ab0fcbe46c9e3a92ef9
samuelnoguchi <samuelrnoguchi@gmail.com>
2019-04-05 14:46:00 -0400

    Begin angular project

---
+++ ComeBuyn/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { environment } from './../environments/environment'
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire'
+import { AngularFireDatabaseModule } from '@angular/fire/database' 
+import { AngularFireAuthModule } from '@angular/fire/auth'
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent},
+      { path: 'login', component: LoginComponent }
+    ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ ComeBuyn/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyD2RoXYt6mqtUwN7WWTYXsbcYlwvoUqM_s",
+    authDomain: "comebuyn-210014.firebaseapp.com",
+    databaseURL: "https://comebuyn-210014.firebaseio.com",
+    projectId: "comebuyn-210014",
+    storageBucket: "comebuyn-210014.appspot.com",
+    messagingSenderId: "296449561126"
+  }
+};
