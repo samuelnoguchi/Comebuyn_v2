489a860dc956be9a9b7e2d2eb8ab02b04f1b220a
samuelnoguchi <samuelrnoguchi@gmail.com>
2019-04-05 15:29:29 -0400

    Implement google login

--- ComeBuyn/src/app/navbar/navbar.component.ts
+++ ComeBuyn/src/app/navbar/navbar.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent
+{
 
-  constructor() { }
+  user: firebase.User;
 
-  ngOnInit() {
+  constructor(private afAuth: AngularFireAuth) { 
+    afAuth.authState.subscribe(user => this.user = user);
+  }
+
+  logout(){
+    this.afAuth.auth.signOut();
   }
 
 }
