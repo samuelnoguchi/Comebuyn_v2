17d885e8a52442351c22fadb77bed7671f7c9f61
samuelnoguchi <33236743+samuelnoguchi@users.noreply.github.com>
2019-04-14 18:29:50 -0400

    Template for completed circles

--- ComeBuyn/src/app/shopping/components/completed-orders/completed-orders.component.ts
+++ ComeBuyn/src/app/shopping/components/completed-orders/completed-orders.component.ts
@@ -10,28 +10,57 @@ import { ProductService } from 'shared/services/product.service';
   templateUrl: './completed-orders.component.html',
   styleUrls: ['./completed-orders.component.css']
 })
-export class CompletedOrdersComponent implements OnInit {
+export class CompletedOrdersComponent {
 
   appUser:AppUser;
-  status:boolean[];
-  orderIds:string[];
-  completedCircles:Product[] =[];
+  status:string[];
+  orderIds:string[] = [];
+  completedCircles:Product[];
   completedCircles$:[Observable<Product>];
 
   constructor(private auth:AuthService, private productService:ProductService) {
     this.auth.appUser$.subscribe(appUser=>{
       this.appUser = appUser;
       if(this.appUser.myOrders){
-        this.orderIds = Object.keys(this.appUser.myOrders)
-        console.log(this.orderIds)
+        this.orderIds = Object.keys(this.appUser.myOrders);
+        this.status = Object.values(this.appUser.myOrders);
+        this.getProductInfo();
         
+      }
+    });
+  }
+
+  //Not working
+
+  //Return list of products
+  getProductInfo(){
+    this.completedCircles$ = this.productService.getAllByIds(this.orderIds);
+
+    for (let pIndex = 0; pIndex < this.completedCircles$.length; pIndex++){      
+      this.completedCircles$[pIndex].subscribe(p=>{
+        p.$key = this.orderIds[pIndex]; // Set the product key
+        this.updateProducts(p);
+      });
+    } 
+  } 
+
+  // Must determine if the product has already been added to the products list if it has, update, otherwise, add
+  updateProducts(p:Product){
+    let found = false;
+    // Iterate through existing products in this.activeCircles
+    for(let i=0; i<this.completedCircles$.length; i++){
+      // If the product already exists, set found to true
+      if(this.completedCircles[i].$key ==  p.$key){
+        found = true;
+        this.completedCircles[i] = p;
+      }
     }
-  });
 
-
-   }
-
-  ngOnInit() {
+    // If the product does not already exists, add it to the list
+    if(!found){
+      this.completedCircles.push(p)
+    }
+    console.log(this.completedCircles)
   }
 
 }
