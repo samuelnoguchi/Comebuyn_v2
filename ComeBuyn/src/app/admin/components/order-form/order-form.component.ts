b76a42cfac500411bed0703109c878bd8be2a67d
samuelnoguchi <samuelrnoguchi@gmail.com>
2019-04-22 16:52:25 -0400

    Show shipping info on order form

--- ComeBuyn/src/app/admin/components/order-form/order-form.component.ts
+++ ComeBuyn/src/app/admin/components/order-form/order-form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { OrderService } from 'shared/services/order.service';
 import { Order } from 'shared/models/order';
+import { ShippingInfo } from 'shared/models/shipping-info';
 
 @Component({
   selector: 'app-order-form',
@@ -28,6 +29,7 @@ export class OrderFormComponent {
   };
 
   buyerIds:any[]=[];
+  shippingInfo:ShippingInfo[]=[];
 
   constructor(private orderService:OrderService, private route: ActivatedRoute, private router:Router) {
     this.id = this.route.snapshot.paramMap.get('id');
@@ -36,7 +38,12 @@ export class OrderFormComponent {
       this.order = order;
 
       // show shipping info
-      this.buyerIds = Object.values(order.product.buyers);
+      let buyers = Object.values(order.product.buyers);
+      for (let buyer of buyers){
+        this.buyerIds.push(buyer['id']);
+        this.shippingInfo.push(buyer['shippingInfo']);
+      }
+
       
     });
   }
