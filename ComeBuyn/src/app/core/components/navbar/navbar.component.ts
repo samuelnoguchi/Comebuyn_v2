377239044087d91696e67ff45bbcc47e22e76906
samuelnoguchi <samuelrnoguchi@gmail.com>
2019-04-22 14:33:14 -0400

    add dropdown

--- ComeBuyn/src/app/core/components/navbar/navbar.component.ts
+++ ComeBuyn/src/app/core/components/navbar/navbar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'shared/services/auth.service';
 import { AppUser } from 'shared/models/app-user';
 import { SearchService } from 'app/core/services/search.service';
+import { CategoryService } from 'shared/services/category.service';
 
 @Component({
   selector: 'app-navbar',
@@ -12,11 +13,13 @@ export class NavbarComponent
 {
   appUser: AppUser;
   navbarOpen = false;
+  categories$;
 
-  constructor(private auth: AuthService, private searchService: SearchService) { 
+  constructor(private auth: AuthService, private searchService: SearchService, private categoryService:CategoryService) { 
     auth.appUser$.subscribe(appUser => {
       this.appUser = appUser;
     });
+    this.categories$ = categoryService.getAll();
   }
 
   search(searchContent){
