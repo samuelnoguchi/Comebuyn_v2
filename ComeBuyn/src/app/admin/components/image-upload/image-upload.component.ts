a1b3d4ff1eefd2963f4fcbf0079cd2ca642db80f
samuelnoguchi <33236743+samuelnoguchi@users.noreply.github.com>
2019-05-13 13:22:18 -0400

    start work on updating image cropper

--- ComeBuyn/src/app/admin/components/image-upload/image-upload.component.ts
+++ ComeBuyn/src/app/admin/components/image-upload/image-upload.component.ts
@@ -1,10 +1,8 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, ViewChild } from '@angular/core';
 import { ImageCroppedEvent } from 'ngx-image-cropper';
-import { AngularFireStorage } from '@angular/fire/storage';
 import { AngularFireDatabase } from '@angular/fire/database';
 import { Router } from '@angular/router';
-import { routerNgProbeToken } from '@angular/router/src/router_module';
-
+import { ImageCropperComponent } from 'ngx-image-cropper';
 
 @Component({
   selector: 'image-upload',
@@ -14,17 +12,33 @@ import { routerNgProbeToken } from '@angular/router/src/router_module';
 export class ImageUploadComponent {
     
   @Output() image = new EventEmitter<string>();
+  @ViewChild(ImageCropperComponent) imageCropper: ImageCropperComponent;
 
+  imagePresent: boolean = false;
   imageChangedEvent: any = '';
   croppedImage: any = '';
+
+  test = null;
 
   constructor(private db: AngularFireDatabase, private router: Router) { }
 
   fileChangeEvent(event: any): void {
       this.imageChangedEvent = event;
   }
+
   imageCropped(event: ImageCroppedEvent) {
       this.croppedImage = event.base64;
-      this.image.emit(this.croppedImage);
+      
   }
+
+  imageLoaded(){
+    this.imagePresent = true;
+  }
+
+  cropImage(){
+    this.image.emit(this.croppedImage);
+    console.log(this.croppedImage);
+    this.imageCropper.imageFileChanged = null;
+  }
+
 }
